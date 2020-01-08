34dfc3bc2b9dee11b15dda4c347c3a28f257946b
Miguel Carlos Honorio <miguel@prosulti.com.br>
2020-01-08 10:42:22 -0300

    correcado do cors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //**
 // database setup
